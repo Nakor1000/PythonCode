82ca4986f07a5ac41cd75310338e6e179bfaed2c
Anthony Young <ay4554@mcla.edu>
2018-01-23 00:37:44 +0000

    Using comments

---
+++ Comments.py
@@ -0,0 +1,11 @@
+import math
+
+#Programmed by Anthony Young
+#This program calculates the volume of a cone.
+#1/21/18 for my python class
+
+radius = 10
+height = 44
+#Calculate the volume of a cone. 
+volume = math.pi*(radius*radius)*(height/3.0)
+print volume
