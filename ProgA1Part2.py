cd67d2512e8636b3824e4df48ce5f6e500fb4f4b
Anthony Young <ay4554@mcla.edu>
2018-01-25 01:32:40 +0000

    HomeworkA1

--- ProgA1Part2.py
+++ ProgA1Part2.py
@@ -3,13 +3,13 @@
 # I plan to continue working on it to allow the use of random sequences, but for now, this meets the requirments,
 # of the assignment. :(
 print 'ACTGATCGATTACGTATAGTAGAATTCTATCATACATATATATCGATGCGTTCAT'
-s2 = 'ACTGATCGATTACGTATAGTAGAATTCTATCATACATATATATCGATGCGTTCAT'
+seq = 'ACTGATCGATTACGTATAGTAGAATTCTATCATACATATATATCGATGCGTTCAT'
 print 'Length of Sequence -',
-print len(s2)
-loc = s2.find('GAATTC')
+print len(seq)
+loc = seq.find('GAATTC')
 print "Location of Restriction site -",
 print loc+1
 print "Fragment Length Before Restriction Site -",
-print len(s2[0:loc+1])
+print len(seq[0:loc+1])
 print "Fragment Length After Restriction Site -",
-print len(s2[loc+1:55])
+print len(seq[loc+1:len(seq)])
