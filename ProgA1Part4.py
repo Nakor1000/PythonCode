fe1e57ea39ad10ff6b6b15bfce640ad6f5e873a1
Anthony Young <ay4554@mcla.edu>
2018-01-25 00:46:17 +0000

    HomeworkA1

--- ProgA1Part4.py
+++ ProgA1Part4.py
@@ -1,4 +1,14 @@
-print "ATCGATCGATCGATCGACTGACTAGTCATAGCTATGCATGTAGCTACTCGATCGATCGATCGATCGATCGATCGATCGATCGATCATGCTATCATCGATCGATATCGATGCATCGACTACTAT"
+seq = "ATCGATCGATCGATCGACTGACTAGTCATAGCTATGCATGTAGCTACTCGATCGATCGATCGATCGATCGATCGATCGATCGATCATGCTATCATCGATCGATATCGATGCATCGACTACTAT"
+print "Length of Sequence -",
+print len(seq)
+print "Exons are in uppercase letters."
+print "Introns are in lowercase letters."
+loc1 = str(seq[0:63]).upper()
+loc2 = str(seq[63:91]).lower()
+loc3 = str(seq[91:len(seq)]).upper()
+print '%s' % loc1+loc2+loc3
+
+
 
 
 
